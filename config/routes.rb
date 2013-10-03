73248390251ce2384fdb6c362e5ba33075022d12
Amanda Neumann <amandaneumann@me.com>
2013-10-04 08:57:41 +1000

    add contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 LearnRails::Application.routes.draw do
+  resources :contacts, only: [:new, :create]
   root to: 'visitors#new'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
